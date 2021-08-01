a1b651f8cfae74d84a534eeefc8c838e33eff95e
trungpv <trungpv@trungpvs-MacBook-Pro-2.local>
2021-08-01 11:39:16 +0700

    GA

--- blog.config.js
+++ blog.config.js
@@ -32,7 +32,7 @@ const BLOG = {
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-C0PB8CMHEB' // e.g: G-XXXXXXXXXX
+      measurementId: 'UA-176269057-5' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
